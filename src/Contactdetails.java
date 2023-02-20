a583042b4a24bc180ec1ac29d444bfdda9fece88
Huzaifa Sabahuddin <huzaifasabah@gmail.com>
2023-02-20 11:08:01 +0000

    LAB 4

--- src/Contactdetails.java
+++ src/Contactdetails.java
@@ -4,7 +4,7 @@ public class Contactdetails {
         Contact contact2 = new Contact("Stevie Wonder", "10 Music Avenue", "1010101010");
         Contact contact3 = new Contact("Gennaro Capuozzo", "1,Four Days of Naples Square", "1111111111");
 
-        System.out.println("Contacts List");
+        System.out.println("Contacts List: ");
         contact1.printDetails();
         System.out.println();
         contact2.printDetails();
